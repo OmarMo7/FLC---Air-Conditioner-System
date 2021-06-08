f2993fcd54729a73a80f5725aff46f2d9e5004aa
OmarMo7 <omar7.first@gmail.com>
2021-06-09 01:21:00 +0200

    Constructing UT function

--- main.py
+++ main.py
@@ -3,5 +3,20 @@ DT = input("Enter the difference in temperatue between in and out: ")
 EV = input("Enter the electric volt value: ")
 
 
-
-
+def UT_Calculator(UT):
+    Low = 0
+    Medium = 0
+    High = 0
+    if (UT > 16 and UT < 22):
+        Low = 1
+    if(UT > 22 and UT < 25):
+        Low = (25-UT)/3
+    if(UT > 22 and UT < 25):
+        Medium = (UT-22)/3
+    if(UT > 25 and UT < 28):
+        Medium = (28 - UT)/3
+    if(UT > 25 and UT < 28):
+        High = (UT-25)/3
+    if(UT > 28 and UT < 34):
+        High = 1
+    return Low, Medium, High
