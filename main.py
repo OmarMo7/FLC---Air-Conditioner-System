2db0c9765d37509fdc29dd5f6b1657ad00a28462
OmarMo7 <omar7.first@gmail.com>
2021-06-09 01:40:42 +0200

    Constructing DT function & adding elif stats

--- main.py
+++ main.py
@@ -9,14 +9,41 @@ def UT_Calculator(UT):
     High = 0
     if (UT > 16 and UT < 22):
         Low = 1
-    if(UT > 22 and UT < 25):
+    elif(UT > 22 and UT < 25):
         Low = (25-UT)/3
     if(UT > 22 and UT < 25):
         Medium = (UT-22)/3
-    if(UT > 25 and UT < 28):
+    elif(UT > 25 and UT < 28):
         Medium = (28 - UT)/3
     if(UT > 25 and UT < 28):
         High = (UT-25)/3
-    if(UT > 28 and UT < 34):
+    elif(UT > 28 and UT < 34):
         High = 1
     return Low, Medium, High
+
+
+def DT_Calculator(DT):
+    Negative = 0
+    Zero = 0
+    Positive = 0
+    Large = 0
+    if (DT > -1 and DT < -0.9):
+        Negative = 1
+    elif(DT > -0.9 and DT < 0):
+        Negative = -0.9 * DT
+    if(DT > -0.5 and DT < 0):
+        Zero = 2*(DT + 0.5)
+    elif(DT > 0 and DT < 0.5):
+        Zero = (28 - DT)/3
+    if(DT > 0 and DT < 1):
+        Positive = DT
+    elif(DT > 1 and DT < 2):
+        Positive = 2 - DT
+    if(DT > 1 and DT < 2):
+        Large = 1 - DT
+    elif(DT > 2 and DT < 3):
+        Large = 1
+    return Negative, Zero, Positive, Large
+
+
+
