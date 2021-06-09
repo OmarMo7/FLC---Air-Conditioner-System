8638ae39e68ef3868c39870ad2ad88f79a9316cf
OmarMo7 <omar7.first@gmail.com>
2021-06-10 00:06:37 +0200

    Extracting combinations to work on from rule base

--- main.py
+++ main.py
@@ -7,17 +7,15 @@ def UT_Calculator(UT):
     Low = 0
     Medium = 0
     High = 0
-    if (UT > 16 and UT < 22):
+    if (UT >= 16 and UT <= 22):
         Low = 1
-    elif(UT > 22 and UT < 25):
+    elif(UT >= 22 and UT <= 25):
         Low = (25-UT)/3
-    if(UT > 22 and UT < 25):
         Medium = (UT-22)/3
-    elif(UT > 25 and UT < 28):
+    if(UT >= 25 and UT <= 28):
         Medium = (28 - UT)/3
-    if(UT > 25 and UT < 28):
         High = (UT-25)/3
-    elif(UT > 28 and UT < 34):
+    elif(UT >= 28 and UT <= 34):
         High = 1
     return Low, Medium, High
 
@@ -27,21 +25,19 @@ def DT_Calculator(DT):
     Zero = 0
     Positive = 0
     Large = 0
-    if (DT > -1 and DT < -0.9):
+    if (DT >= -1 and DT <= -0.9):
         Negative = 1
-    elif(DT > -0.9 and DT < 0):
+    elif(DT >= -0.9 and DT <= 0):
         Negative = -0.9 * DT
-    if(DT > -0.5 and DT < 0):
         Zero = 2*(DT + 0.5)
-    elif(DT > 0 and DT < 0.5):
-        Zero = (28 - DT)/3
-    if(DT > 0 and DT < 1):
+    if(DT >= 0 and DT <= 0.5):
+        Zero = 2*(0.5 - DT)
+    if(DT >= 0 and DT <= 1):
         Positive = DT
-    elif(DT > 1 and DT < 2):
+    if(DT >= 1 and DT <= 2):
         Positive = 2 - DT
-    if(DT > 1 and DT < 2):
         Large = 1 - DT
-    elif(DT > 2 and DT < 3):
+    elif(DT >= 2 and DT <= 3):
         Large = 1
     return Negative, Zero, Positive, Large
 
@@ -50,13 +46,13 @@ def EV_Calculator(EV):
     Low = 0
     High = 0
 
-    if (EV > 130 and EV < 160):
+    if (EV >= 130 and EV <= 160):
         Low = 1
-    elif (EV > 160 and EV < 180):
+    elif (EV >= 160 and EV <= 180):
         Low = (180 - EV)/20
-    if (EV > 170 and EV < 190):
+    if (EV >= 170 and EV <= 190):
         High = (EV - 170)/20
-    elif (EV > 190 and EV < 220):
+    elif (EV >= 190 and EV <= 220):
         High = 1
     return Low, High
 
@@ -64,5 +60,43 @@ def EV_Calculator(EV):
 Fuzzy_UT = UT_Calculator(UT)
 Fuzzy_DT = DT_Calculator(DT)
 Fuzzy_EV = EV_Calculator(EV)
+Pos_UT = []
+Pos_DT = []
+Pos_EV = []
 
-print(Fuzzy_UT, Fuzzy_DT, Fuzzy_EV)
+for value in Fuzzy_UT:
+    if (value != 0):
+        if (not Pos_UT.__contains__(Fuzzy_UT.index(value))):
+            Pos_UT.append(Fuzzy_UT.index(value))
+        else:
+            lastIndex = Fuzzy_UT.index(value)
+            Pos_UT.append(Fuzzy_UT.index(value, lastIndex+1))
+for value in Fuzzy_DT:
+    if (value != 0):
+        if (not Pos_DT.__contains__(Fuzzy_DT.index(value))):
+            Pos_DT.append(Fuzzy_DT.index(value))
+        else:
+            lastIndex = Fuzzy_DT.index(value)
+            Pos_DT.append(Fuzzy_DT.index(value, lastIndex+1))
+for value in Fuzzy_EV:
+    if (value != 0):
+        if (not Pos_EV.__contains__(Fuzzy_EV.index(value))):
+            Pos_EV.append(Fuzzy_EV.index(value))
+        else:
+            lastIndex = Fuzzy_EV.index(value)
+            Pos_EV.append(Fuzzy_EV.index(value, lastIndex+1))
+
+print(Pos_UT,
+      Pos_DT,
+      Pos_EV)
+
+
+Combinations = []
+for i in Pos_UT:
+    for j in Pos_DT:
+        for k in Pos_EV:
+            Combinations.append([i, j, k])
+
+print(Combinations)
+# print(["Opt, Pos, Low"], ["Opt, Pos, High"],
+#       ["High, Pos, Low"], ["High, Pos, High"])
