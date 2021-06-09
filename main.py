5d01d69ff1e631730b2691c92424c134d19f51b0
Youssef Sayed <47215226+youssef893@users.noreply.github.com>
2021-06-10 00:36:59 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,68 +1,30 @@
-UT = int(input("Enter the user temperature value: "))
-DT = float(input("Enter the difference in temperatue between in and out: "))
-EV = int(input("Enter the electric volt value: "))
+import pandas as pd
 
 
-def UT_Calculator(UT):
-    Low = 0
-    Medium = 0
-    High = 0
-    if (UT > 16 and UT < 22):
-        Low = 1
-    elif(UT > 22 and UT < 25):
-        Low = (25-UT)/3
-    if(UT > 22 and UT < 25):
-        Medium = (UT-22)/3
-    elif(UT > 25 and UT < 28):
-        Medium = (28 - UT)/3
-    if(UT > 25 and UT < 28):
-        High = (UT-25)/3
-    elif(UT > 28 and UT < 34):
-        High = 1
-    return Low, Medium, High
+def read_filename():
+    base_rules = pd.read_excel('BaseRules.xlsx', engine='openpyxl')
+    base_rules = base_rules.drop([24])
+    print(base_rules)
+    return base_rules
 
 
-def DT_Calculator(DT):
-    Negative = 0
-    Zero = 0
-    Positive = 0
-    Large = 0
-    if (DT > -1 and DT < -0.9):
-        Negative = 1
-    elif(DT > -0.9 and DT < 0):
-        Negative = -0.9 * DT
-    if(DT > -0.5 and DT < 0):
-        Zero = 2*(DT + 0.5)
-    elif(DT > 0 and DT < 0.5):
-        Zero = (28 - DT)/3
-    if(DT > 0 and DT < 1):
-        Positive = DT
-    elif(DT > 1 and DT < 2):
-        Positive = 2 - DT
-    if(DT > 1 and DT < 2):
-        Large = 1 - DT
-    elif(DT > 2 and DT < 3):
-        Large = 1
-    return Negative, Zero, Positive, Large
+def check_output(base_rules, inputs):
+    output = []
+    for i in range(len(base_rules)):
+        if base_rules.iloc[i][0] == inputs[0] and base_rules.iloc[i][1] == inputs[1] \
+                and base_rules.iloc[i][2] == inputs[2]:
+            output.append(base_rules.iloc[i][3])
+            output.append(base_rules.iloc[i][4])
+            print(output)
+            return output
+    return "These inputs are not allowed"
 
 
-def EV_Calculator(EV):
-    Low = 0
-    High = 0
-
-    if (EV > 130 and EV < 160):
-        Low = 1
-    elif (EV > 160 and EV < 180):
-        Low = (180 - EV)/20
-    if (EV > 170 and EV < 190):
-        High = (EV - 170)/20
-    elif (EV > 190 and EV < 220):
-        High = 1
-    return Low, High
+def main():
+    base_rules = read_filename()
+    check_output(base_rules,  [0.0,  1.0,  1.0])
 
 
-Fuzzy_UT = UT_Calculator(UT)
-Fuzzy_DT = DT_Calculator(DT)
-Fuzzy_EV = EV_Calculator(EV)
+if __name__ == '__main__':
+    main()
 
-print(Fuzzy_UT, Fuzzy_DT, Fuzzy_EV)
