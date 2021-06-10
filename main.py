d8204ef2c3d699f59482cd5fbd78a9114d22898f
Omar Mostafa <43853632+OmarMo7@users.noreply.github.com>
2021-06-10 05:59:50 +0200

    The complete code with error-free

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import pandas as pd
+
 UT = int(input("Enter the user temperature value: "))
 DT = float(input("Enter the difference in temperatue between in and out: "))
 EV = int(input("Enter the electric volt value: "))
-
-
 def UT_Calculator(UT):
     Low = 0
     Medium = 0
     High = 0
     if (UT >= 16 and UT <= 22):
+    if 16 <= UT <= 22:
         Low = 1
     elif(UT >= 22 and UT <= 25):
         Low = (25-UT)/3
@@ -17,9 +17,15 @@ def UT_Calculator(UT):
         Medium = (28 - UT)/3
         High = (UT-25)/3
     elif(UT >= 28 and UT <= 34):
+    elif 22 <= UT <= 25:
+        Low = (25 - UT) / 3
+        Medium = (UT - 22) / 3
+    if 25 <= UT <= 28:
+        Medium = (28 - UT) / 3
+        High = (UT - 25) / 3
+    elif 28 <= UT <= 34:
         High = 1
     return Low, Medium, High
-
 
 def DT_Calculator(DT):
     Negative = 0
@@ -27,36 +33,48 @@ def DT_Calculator(DT):
     Positive = 0
     Large = 0
     if (DT >= -1 and DT <= -0.9):
+    if -1 <= DT <= -0.9:
         Negative = 1
     elif(DT >= -0.9 and DT <= 0):
+    elif -0.9 <= DT <= 0:
         Negative = -0.9 * DT
         Zero = 2*(DT + 0.5)
     if(DT >= 0 and DT <= 0.5):
         Zero = 2*(0.5 - DT)
     if(DT >= 0 and DT <= 1):
+        Zero = 2 * (DT + 0.5)
+    if 0 <= DT <= 0.5:
+        Zero = 2 * (0.5 - DT)
+    if 0 <= DT <= 1:
         Positive = DT
     if(DT >= 1 and DT <= 2):
+    if 1 <= DT <= 2:
         Positive = 2 - DT
         Large = 1 - DT
     elif(DT >= 2 and DT <= 3):
+    elif 2 <= DT <= 3:
         Large = 1
     return Negative, Zero, Positive, Large
-
 
 def EV_Calculator(EV):
     Low = 0
     High = 0
 
     if (EV >= 130 and EV <= 160):
+    if 130 <= EV <= 160:
         Low = 1
     elif (EV >= 160 and EV <= 180):
         Low = (180 - EV)/20
     if (EV >= 170 and EV <= 190):
         High = (EV - 170)/20
     elif (EV >= 190 and EV <= 220):
+    elif 160 <= EV <= 180:
+        Low = (180 - EV) / 20
+    if 170 <= EV <= 190:
+        High = (EV - 170) / 20
+    elif 190 <= EV <= 220:
         High = 1
     return Low, High
-
 
 def Get_Combinations(Fuzzy_UT, Fuzzy_DT, Fuzzy_EV):
     Pos_UT = []
@@ -65,25 +83,34 @@ def Get_Combinations(Fuzzy_UT, Fuzzy_DT, Fuzzy_EV):
     for value in Fuzzy_UT:
         if (value != 0):
             if (not Pos_UT.__contains__(Fuzzy_UT.index(value))):
+        if value != 0:
+            if not Pos_UT.__contains__(Fuzzy_UT.index(value)):
                 Pos_UT.append(Fuzzy_UT.index(value))
             else:
                 lastIndex = Fuzzy_UT.index(value)
                 Pos_UT.append(Fuzzy_UT.index(value, lastIndex+1))
+                Pos_UT.append(Fuzzy_UT.index(value, lastIndex + 1))
 
     for value in Fuzzy_DT:
         if (value != 0):
             if (not Pos_DT.__contains__(Fuzzy_DT.index(value))):
+        if value != 0:
+            if not Pos_DT.__contains__(Fuzzy_DT.index(value)):
                 Pos_DT.append(Fuzzy_DT.index(value))
             else:
                 lastIndex = Fuzzy_DT.index(value)
                 Pos_DT.append(Fuzzy_DT.index(value, lastIndex+1))
+                Pos_DT.append(Fuzzy_DT.index(value, lastIndex + 1))
     for value in Fuzzy_EV:
         if (value != 0):
             if (not Pos_EV.__contains__(Fuzzy_EV.index(value))):
+        if value != 0:
+            if not Pos_EV.__contains__(Fuzzy_EV.index(value)):
                 Pos_EV.append(Fuzzy_EV.index(value))
             else:
                 lastIndex = Fuzzy_EV.index(value)
                 Pos_EV.append(Fuzzy_EV.index(value, lastIndex+1))
+                Pos_EV.append(Fuzzy_EV.index(value, lastIndex + 1))
 
     Combinations = []
     for i in Pos_UT:
@@ -91,8 +118,6 @@ def Get_Combinations(Fuzzy_UT, Fuzzy_DT, Fuzzy_EV):
             for k in Pos_EV:
                 Combinations.append([i, j, k])
     return Combinations
-
-
 def Get_Minimums(Combinations, Fuzzy_UT,
                  Fuzzy_DT,
                  Fuzzy_EV):
@@ -100,15 +125,11 @@ def Get_Minimums(Combinations, Fuzzy_UT,
     for i in Combinations:
         min_values.append(min(Fuzzy_UT[i[0]], Fuzzy_DT[i[1]], Fuzzy_EV[i[2]]))
     return min_values
-
-
 def Read_Filename():
     base_rules = pd.read_excel('BaseRules.xlsx', engine='openpyxl')
     base_rules = base_rules.drop([24])
     print(base_rules)
     return base_rules
-
-
 def Check_Output(base_rules, inputs):
     output = []
     for i in range(len(base_rules)):
@@ -125,6 +146,10 @@ def Defuzzification(output_values, min_values):
     low, medium, fast = 0, 0, 0
     for i in range(len(min_values)):
         for j in range(2):
+    defuzzification_values = []
+
+    for j in range(2):
+        for i in range(len(min_values)):
             if output_values[i][j] == 0:
                 low += min_values[i] * 30
             elif output_values[i][j] == 1:
@@ -132,14 +157,17 @@ def Defuzzification(output_values, min_values):
             elif output_values[i][j] == 2:
                 fast += min_values[i] * 90
     return (low + medium + fast) / sum(min_values)
+        defuzzification_values.append((low + medium + fast) / sum(min_values))
+        low, medium, fast = 0, 0, 0
+    return defuzzification_values
 
 
 def main():
+    outputs = []
     base_rules = Read_Filename()
     Fuzzy_UT = UT_Calculator(UT)
     Fuzzy_DT = DT_Calculator(DT)
     Fuzzy_EV = EV_Calculator(EV)
-
     Combinations = Get_Combinations(Fuzzy_UT,
                                     Fuzzy_DT,
                                     Fuzzy_EV)
@@ -147,8 +175,18 @@ def main():
                         Fuzzy_DT,
                         Fuzzy_EV)
     Outputs = Check_Output(base_rules, Combinations)
+    minimum_values = Get_Minimums(Combinations, Fuzzy_UT,
+                                  Fuzzy_DT,
+                                  Fuzzy_EV)
+    for i in Combinations:
+        output = Check_Output(base_rules, i)
+        outputs.append(output)
 
     Defuzzification(Outputs, Mins)
+    values = Defuzzification(outputs, minimum_values)
+    print('\n')
+    print('Compressor Speed =', values[0])
+    print('Fan Speed =', values[0])
 
 
 if __name__ == '__main__':
